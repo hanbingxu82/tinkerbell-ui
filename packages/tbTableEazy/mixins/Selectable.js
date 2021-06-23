7d848e3bc2e4a8800cdd5ca225c16d94d03c10cb
hanbingxu <hbx831@126.com>
2021-06-23 10:31:33 +0800

    feat:2021-06-23 调整分页组件以及表格组件

---
+++ packages/tbTableEazy/mixins/Selectable.js
@@ -0,0 +1,97 @@
+/*
+ * @Author: your name
+ * @Date: 2021-06-22 16:24:34
+ * @LastEditTime: 2021-06-22 19:38:55
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /hx/packages/tbTableEazy/mixins/Selectable.js
+ */
+const Selectable = ({ rows = 'rows' } = {}) => ({
+    props: {
+      selectable: Boolean
+    },
+  
+    data () {
+      return {
+        selectedRows: [],
+        lastClicked: undefined
+      }
+    },
+  
+    computed: {
+      isSelectedAll () {
+        const isNotEmpty = !!this[rows].length
+        const isEqualsLength = isNotEmpty && this[rows].length === this.selectedRows.length
+        const isSelectedAll = isEqualsLength && this[rows].every((row) => {
+          return this.selectedRows.includes(row)
+        })
+        return isSelectedAll
+      }
+    },
+  
+    watch: {
+      rows () {
+        this.selectedRows = []
+      }
+    },
+  
+    methods: {
+      /**
+       * Check if a row is selected.
+       * @param {object} row
+       * @returns {boolean}
+       */
+      isSelected (row) {
+        const isSelected = !!this.selectedRows.find((selected) => selected === row)
+        return isSelected
+      },
+  
+      /**
+       * Set row active.
+       * @param {object} row
+       * @param {object} event
+       */
+      select (row, event) {
+        if (event.shiftKey && this.lastClicked !== row) {
+          this.multipleSelect(row)
+        } else {
+          this.selectedRows = this.isSelected(row)
+            ? this.selectedRows.filter((selected) => selected !== row)
+            : [ ...this.selectedRows, row ]
+        }
+        this.lastClicked = row
+        this.emitSelected()
+      },
+  
+      /**
+       * Set multiple rows active.
+       * @param {object} row
+       */
+      multipleSelect (row) {
+        const s1 = this[rows].indexOf(row)
+        const s2 = this[rows].indexOf(this.lastClicked)
+        const [start, end] = [s1, s2].sort()
+        const range = Array(end - start + 1).fill().map((_, index) => start + index)
+        this.selectedRows = this[rows].filter((_, index) => range.includes(index))
+      },
+  
+      /**
+       * Set all rows active.
+       * @param {Event} event
+       */
+      selectAll (event) {
+        this.selectedRows = this.isSelectedAll ? [] : [ ...this[rows] ]
+        this.emitSelected()
+      },
+  
+      /**
+       * Emit selected rows.
+       */
+      emitSelected () {
+        this.$emit('select', this.selectedRows)
+      }
+    }
+  })
+  
+  export default Selectable
+  
