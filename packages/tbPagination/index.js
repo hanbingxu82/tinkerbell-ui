e8cba4073e85be41d990eb96866adbbc0295ce22
hanbingxu <hbx831@126.com>
2021-06-23 09:47:37 +0800

    feat:2021-06-23 调整分页组件以及表格组件

--- packages/tbPagination/index.js
+++ packages/tbPagination/index.js
@@ -1,8 +1,16 @@
 /*
  * @Author: your name
  * @Date: 2021-05-27 16:29:38
- * @LastEditTime: 2021-05-27 16:29:42
+ * @LastEditTime: 2021-06-22 15:08:19
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /hx/packages/tbPagination/index.js
  */
+import tbPagination from "./main.vue";
+
+// 为组件添加 install 方法，用于按需引入
+tbPagination.install = function(Vue) {
+  Vue.component(tbPagination.name, tbPagination);
+};
+
+export default tbPagination;
