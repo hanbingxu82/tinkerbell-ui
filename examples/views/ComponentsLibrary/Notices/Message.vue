727fb5cec31e7f7ef7dfb4f54b6af282e8b5b9ed
hanbingxu <hbx831@126.com>
2021-07-01 16:17:13 +0800

    fix:2021-07-01 完善tinkerbell-ui组件库官网 Message

--- examples/views/ComponentsLibrary/Notices/Message.vue
+++ examples/views/ComponentsLibrary/Notices/Message.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2021-07-01 13:38:16
- * @LastEditTime: 2021-07-01 15:39:20
+ * @LastEditTime: 2021-07-01 16:09:01
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /hx/examples/views/ComponentsLibrary/Notices.vue/Message.vue
@@ -10,42 +10,26 @@
 <template>
   <div>
     <h2
-      id="loadingBar-ding-bu-jia-zai"
+      id="message-ding-bu-jia-zai"
       style='box-sizing: border-box; margin: 0px; padding: 10px 0px; font-size: 28px; font-weight: 400; color: rgb(31, 47, 61); border-bottom: 1px solid rgb(197, 217, 232); font-family: -apple-system, system-ui, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"; white-space: normal; background-color: rgb(255, 255, 255);'
     >
-      <a href="#loadingBar-ding-bu-jia-zai" class="header-anchor" style="box-sizing: border-box; color: rgb(16, 137, 255); text-decoration-line: none;">¶</a>&nbsp;LoadingBar 顶部加载
+      <a href="#message-ding-bu-jia-zai" class="header-anchor" style="box-sizing: border-box; color: rgb(16, 137, 255); text-decoration-line: none;">¶</a>&nbsp;Message 通知
     </h2>
     <p
       style='box-sizing: border-box; margin-top: 1em; margin-bottom: 1em; padding: 0px; color: rgba(0, 0, 0, 0.85); font-family: -apple-system, system-ui, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"; font-size: 14px; white-space: normal; background-color: rgb(255, 255, 255);'
     >
-      全局创建一个显示页面加载、异步请求、文件上传等的加载进度条。
+      轻量级的信息反馈组件，在顶部居中显示，并自动消失。有多种不同的提示状态可选择。
     </p>
     <h3
-      id="lu-you-shi-yong"
+      id="ji-chu-yong-fa"
       style='box-sizing: border-box; margin: 36px 0px 20px; padding: 0px; font-size: 22px; font-weight: 400; color: rgb(31, 47, 61); font-family: -apple-system, system-ui, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"; white-space: normal; background-color: rgb(255, 255, 255);'
     >
-      <a href="#lu-you-shi-yong" class="header-anchor" style="box-sizing: border-box; color: rgb(16, 137, 255); text-decoration-line: none;">¶</a>&nbsp;在路由中使用
+      <a href="#ji-chu-yong-fa" class="header-anchor" style="box-sizing: border-box; color: rgb(16, 137, 255); text-decoration-line: none;">¶</a>&nbsp; 基础用法
     </h3>
     <p
       style='box-sizing: border-box; margin-top: 1em; margin-bottom: 1em; padding: 0px; color: rgba(0, 0, 0, 0.85); font-family: -apple-system, system-ui, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"; font-size: 14px; white-space: normal; background-color: rgb(255, 255, 255);'
     >
-      LoadingBar 只会在全局创建一个，因此在任何位置调用的方法都会控制这同一个组件。 主要使用场景是路由切换和Ajax，因为这两者都不能拿到精确的进度，LoadingBar 会模拟进度，具体见API。
-    </p>
-
-    <pre slot="highlight" v-highlight>
-        <code class="javascript"  v-text="html1">
-        </code>
-      </pre>
-    <h3
-      id="dai-ma-shi-li"
-      style='box-sizing: border-box; margin: 36px 0px 20px; padding: 0px; font-size: 22px; font-weight: 400; color: rgb(31, 47, 61); font-family: -apple-system, system-ui, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"; white-space: normal; background-color: rgb(255, 255, 255);'
-    >
-      <a href="#dai-ma-shi-li" class="header-anchor" style="box-sizing: border-box; color: rgb(16, 137, 255); text-decoration-line: none;">¶</a>&nbsp; 代码实例
-    </h3>
-    <p
-      style='box-sizing: border-box; margin-top: 1em; margin-bottom: 1em; padding: 0px; color: rgba(0, 0, 0, 0.85); font-family: -apple-system, system-ui, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"; font-size: 14px; white-space: normal; background-color: rgb(255, 255, 255);'
-    >
-      点击 Start 开始进度，点击 Done 结束。在调用start()方法后，组件会自动模拟进度，当调用done()或error()时，补全进度并自动消失。
+      基本用法，默认在 3 秒后关闭
     </p>
     <p>
       <br />
@@ -54,25 +38,27 @@
     <Demo>
       点击 Config 之后再次查看配置项一般默认即可
       <div slot="source">
-        <tb-button style="margin-right:10px" @click="start">开始</tb-button>
-        <tb-button style="margin-right:10px" @click="end">结束</tb-button>
-        <tb-button @click="error">错误</tb-button>
+        <tb-button @click="$message('this is default message')">默认提示</tb-button>
+        <tb-button type="primary" @click="$message({ type: 'primary', content: 'primary 的提示' })">primary</tb-button>
+        <tb-button type="success" @click="$message({ type: 'success', content: 'success 的提示' })">success</tb-button>
+        <tb-button type="warning" @click="$message({ type: 'warning', content: 'warning 的提示' })">warning</tb-button>
+        <tb-button type="danger" @click="$message({ type: 'danger', content: 'danger 的提示' })">danger</tb-button>
       </div>
       <pre slot="highlight" v-highlight>
-        <code class="javascript"  v-text="html2">
+        <code class="javascript"  v-text="html1">
         </code>
       </pre>
     </Demo>
     <h3
-      id="chang-gui-pei-zhi"
+      id="pei-zhi-can-shu"
       style='box-sizing: border-box; margin: 36px 0px 20px; padding: 0px; font-size: 22px; font-weight: 400; color: rgb(31, 47, 61); font-family: -apple-system, system-ui, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"; white-space: normal; background-color: rgb(255, 255, 255);'
     >
-      <a href="#chang-gui-pei-zhi" class="header-anchor" style="box-sizing: border-box; color: rgb(16, 137, 255); text-decoration-line: none;">¶</a>&nbsp;常规配置
+      <a href="#pei-zhi-can-shu" class="header-anchor" style="box-sizing: border-box; color: rgb(16, 137, 255); text-decoration-line: none;">¶</a>&nbsp;配置参数
     </h3>
     <p
       style='box-sizing: border-box; margin-top: 1em; margin-bottom: 1em; padding: 0px; color: rgba(0, 0, 0, 0.85); font-family: -apple-system, system-ui, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"; font-size: 14px; white-space: normal; background-color: rgb(255, 255, 255);'
     >
-      提供 LoadingBar 的全局配置，使用方法如下
+      可以设置配置参数来渲染
     </p>
     <p>
       <br />
@@ -80,10 +66,33 @@
 
     <Demo>
       <div slot="source">
-        <tb-button style="margin-right:10px" @click="setSpeed">设置speed速度</tb-button>
-        <tb-button style="margin-right:10px" @click="setSpinner">设置spinner动画效果</tb-button>
-        <tb-button style="margin-right:10px" @click="setPercentNum">设置percentNum每次加载的比例</tb-button>
-        <tb-button style="margin-right:10px" @click="setShowSpinner">设置是否显示spinner</tb-button>
+        <tb-button type="default" @click="$message({ content: '我10秒消失', duration: 10 })">我10秒消失</tb-button>
+        <tb-button type="default" @click="$message({ content: '我是可关闭的的提示', duration: 10, closable: true })">可关闭的</tb-button>
+        <tb-button type="default" @click="$message({ content: '我永远不会关闭除非手动', duration: 0, closable: true, zIndex: 3000 })">不会关闭</tb-button>
+      </div>
+      <pre slot="highlight" v-highlight>
+        <code class="javascript"  v-text="html2">
+        </code>
+      </pre>
+    </Demo>
+    <h3
+      id="render-han-shu-diao-yong"
+      style='box-sizing: border-box; margin: 36px 0px 20px; padding: 0px; font-size: 22px; font-weight: 400; color: rgb(31, 47, 61); font-family: -apple-system, system-ui, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"; white-space: normal; background-color: rgb(255, 255, 255);'
+    >
+      <a href="#render-han-shu-diao-yong" class="header-anchor" style="box-sizing: border-box; color: rgb(16, 137, 255); text-decoration-line: none;">¶</a>&nbsp;render函数调用
+    </h3>
+    <p
+      style='box-sizing: border-box; margin-top: 1em; margin-bottom: 1em; padding: 0px; color: rgba(0, 0, 0, 0.85); font-family: -apple-system, system-ui, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"; font-size: 14px; white-space: normal; background-color: rgb(255, 255, 255);'
+    >
+      render函数调用
+    </p>
+    <p>
+      <br />
+    </p>
+
+    <Demo>
+      <div slot="source">
+        <tb-button type="default" @click="renderFunc">render函数调用</tb-button>
       </div>
       <pre slot="highlight" v-highlight>
         <code class="javascript"  v-text="html3">
@@ -91,25 +100,32 @@
       </pre>
     </Demo>
     <h3
-      id="rowLoadingBarEvent"
+      id="API"
       style='box-sizing: border-box; margin: 36px 0px 20px; padding: 0px; font-size: 22px; font-weight: 400; color: rgb(31, 47, 61); font-family: -apple-system, system-ui, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"; white-space: normal; background-color: rgb(255, 255, 255);'
     >
-      <a href="#rowLoadingBarEvent" class="header-anchor" style="box-sizing: border-box; color: rgb(16, 137, 255); text-decoration-line: none;">¶</a>&nbsp;LoadingBar events
+      <a href="#API" class="header-anchor" style="box-sizing: border-box; color: rgb(16, 137, 255); text-decoration-line: none;">¶</a>&nbsp;API
     </h3>
-    <p>
-      <br />
+    <p
+      style='box-sizing: border-box; margin-top: 1em; margin-bottom: 1em; padding: 0px; color: rgba(0, 0, 0, 0.85); font-family: -apple-system, system-ui, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"; font-size: 14px; white-space: normal; background-color: rgb(255, 255, 255);'
+    >
+      通过直接调用以下方法来使用组件：
     </p>
-    <tb-table-eazy :rows="rowLoadingBarEvent" :cols="colEvent"></tb-table-eazy>
+
+    <pre slot="highlight" v-highlight>
+        <code class="javascript"  v-text="html4">
+        </code>
+      </pre>
+
     <h3
-      id="rowLoadingBarConfig"
+      id="rowMessage"
       style='box-sizing: border-box; margin: 36px 0px 20px; padding: 0px; font-size: 22px; font-weight: 400; color: rgb(31, 47, 61); font-family: -apple-system, system-ui, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"; white-space: normal; background-color: rgb(255, 255, 255);'
     >
-      <a href="#rowLoadingBarConfig" class="header-anchor" style="box-sizing: border-box; color: rgb(16, 137, 255); text-decoration-line: none;">¶</a>&nbsp;LoadingBarConfig props
+      <a href="#rowMessage" class="header-anchor" style="box-sizing: border-box; color: rgb(16, 137, 255); text-decoration-line: none;">¶</a>&nbsp;Message props
     </h3>
     <p>
       <br />
     </p>
-    <tb-table-eazy :rows="rowLoadingBarConfig" :cols="col"></tb-table-eazy>
+    <tb-table-eazy :rows="rowMessage" :cols="col"></tb-table-eazy>
     <RightSmallNav :rightTitle="rightTitle" @goMeowPoint="goMeowPoint" />
   </div>
 </template>
@@ -118,127 +134,113 @@
 import useScroll from "../../../utils/mixins";
 import RightSmallNav from "../../../components/RightSmallNav.vue";
 export default {
-  name: "LoadingBar",
+  name: "Message",
   components: { RightSmallNav },
   data() {
     return {
-      rowLoadingBarEvent: [
+      rowMessage: [
         {
-          Parameters: "start",
-          Explain: "开始从 0 显示进度条，并自动加载进度",
-          Callback: "-",
-        },
-        {
-          Parameters: "end",
-          Explain: "结束进度条，自动补全剩余进度",
-          Callback: "-",
-        },
-        {
-          Parameters: "error",
-          Explain: "以错误的类型结束进度条，自动补全剩余进度",
-          Callback: "-",
-        },
-      ],
-      rowLoadingBarConfig: [
-        {
-          Parameters: "speed",
-          Explain: "加载速度",
-          Types: "Number",
-          Optional: "0-100",
-          Default: "5",
-        },
-
-        {
-          Parameters: "easing",
-          Explain: "spinner加载动画",
+          Parameters: "content",
+          Explain: "提示内容",
           Types: "String",
-          Optional: "linear, ease, cubic-bezier...",
-          Default: "贝萨尔曲线值",
-        },
-        {
-          Parameters: "percentNum",
-          Explain: "每次前进的百分比",
-          Types: "Float",
-          Optional: "0-1",
-          Default: "-",
-        },
-        {
-          Parameters: "showSpinner",
-          Explain: "是否显示spinner",
-          Types: "Boolean",
           Optional: "-",
           Default: "-",
         },
+        {
+          Parameters: "type",
+          Explain: "提示类别(primary,info,success,warning,danger)",
+          Types: "String",
+          Optional: "primary,info,success,warning,danger",
+          Default: "primary",
+        },
+        {
+          Parameters: "render",
+          Explain: "自定义描述内容，使用 Vue 的 Render 函数",
+          Types: "Function",
+          Optional: "-",
+          Default: "-",
+        },
+        {
+          Parameters: "duration",
+          Explain: "自动关闭的延时，单位秒，不关闭可以写 0",
+          Types: "Number",
+          Optional: "-",
+          Default: "3",
+        },
+        {
+          Parameters: "onClose",
+          Explain: "关闭时的回调",
+          Types: "Function",
+          Optional: "—",
+          Default: "—",
+        },
+        {
+          Parameters: "closable",
+          Explain: "是否显示关闭按钮",
+          Types: "Boolean",
+          Optional: "-",
+          Default: "false",
+        },
+        {
+          Parameters: "zIndex",
+          Explain: "显示层级",
+          Types: "Number",
+          Optional: "-",
+          Default: "2000",
+        },
       ],
-      html1: `    import tinkerbellUI from 'tinkerbell-ui'
-      
-    router.beforeEach((to, from, next) => {
-    tinkerbellUI.tbLoadingBar.start();
-    next();
-    });
-    router.afterEach(() => {
-    tinkerbellUI.tbLoadingBar.end();
-    });
+      html1: `    <tb-button @click="$message('this is default message')">默认提示</tb-button>
+    <tb-button type="primary" @click="$message({type:'primary',content:'primary 的提示'})">primary</tb-button>
+    <tb-button type="success" @click="$message({type:'success',content:'success 的提示'})">success</tb-button>
+    <tb-button type="warning" @click="$message({type:'warning',content:'warning 的提示'})">warning</tb-button>
+    <tb-button type="danger" @click="$message({type:'danger',content:'danger 的提示'})">danger</tb-button>
             `,
-      html2: `    <tb-button @click="start">开始</tb-button>
-    <tb-button @click="end">结束</tb-button>
-    <tb-button @click="error">错误</tb-button>
+      html2: `    <tb-button type="default" @click="$message({ content: '我10秒消失', duration: 10 })">我10秒消失</tb-button>
+    <tb-button type="default" @click="$message({ content: '我是可关闭的的提示', duration: 10, closable: true })">可关闭的</tb-button>
+    <tb-button type="default" @click="$message({ content: '我永远不会关闭除非手动', duration: 0, closable: true, zIndex: 3000 })">不会关闭</tb-button>
+              `,
+      html3: `    <template>
+        <tb-button type="default" @click="renderFunc">render函数调用</tb-button>
+    </template>
     <script>
-        export default{
+        export default {
             methods: {
-            start () {
-                this.$loading.start()
-            },
-            end () {
-                this.$loading.end()
-            },
-            error () {
-                this.$loading.error()
+            renderFunc () {
+                this.$message({
+                    render: (h) => {
+                        return h("span", [
+                            "This is created by ",
+                            h("span", {
+                            domProps: {
+                                innerHTML: "render",
+                            },
+                            style: {
+                                color: "red",
+                            },
+                            }),
+                            " function",
+                        ]);
+                    },
+                });
             }
             }
         }
     <\/script>
               `,
-      html3: `    <tb-button @click="setSpeed">设置speed速度</tb-button>
-    <tb-button @click="setSpinner">设置spinner动画效果</tb-button>
-    <tb-button @click="setPercentNum">设置percentNum每次加载的比例</tb-button>
-    <tb-button @click="setShowSpinner">设置是否显示spinner</tb-button>
-    <script>
-        export default{
-            methods: {
-            setSpeed () {
-                this.$loading.config({
-                    speed: 10
-                })
-                },
-                setSpinner () {
-                this.$loading.config({
-                    easing: 'ease'
-                })
-                },
-                setPercentNum () {
-                this.$loading.config({
-                    percentNum: 0.1
-                })
-                },
-                setShowSpinner () {
-                this.$loading.config({
-                    showSpinner: false
-                })
-                }
-            }
-        }
-    <\/script>
+      html4: `    this.$message('这是一条普通的提示')
+
+    this.$message({type:'success',content:'success 的提示'})
+        
+    this.$message({content:'我10秒消失',duration:10})
               `,
 
       //   定义当前传递过来的数据是所有类型的数据
       rightTitle: [
-        { title: "LoadingBar 顶部加载", id: "loadingBar-ding-bu-jia-zai" },
-        { title: "在路由中使用", id: "lu-you-shi-yong" },
-        { title: "代码实例", id: "dai-ma-shi-li" },
-        { title: "常规配置", id: "chang-gui-pei-zhi" },
-        { title: "LoadingBarEvent props", id: "rowLoadingBarEvent" },
-        { title: "LoadingBarConfig props", id: "rowLoadingBarConfig" },
+        { title: "Message 通知", id: "message-ding-bu-jia-zai" },
+        { title: "基础用法", id: "ji-chu-yong-fa" },
+        { title: "配置参数", id: "pei-zhi-can-shu" },
+        { title: "render函数调用", id: "render-han-shu-diao-yong" },
+        { title: "Message props", id: "rowMessage" },
       ],
     };
   },
@@ -258,40 +260,29 @@ export default {
       const containern = window.document.querySelector("#" + id);
       containern.scrollIntoView(true);
     },
-    start() {
-      this.$loading.start();
-    },
-    end() {
-      this.$loading.end();
-    },
-    error() {
-      this.$loading.error();
-    },
-    setSpeed() {
-      this.$loading.config({
-        speed: 10,
-      });
-    },
-    setSpinner() {
-      this.$loading.config({
-        easing: "ease",
-      });
-    },
-    setPercentNum() {
-      this.$loading.config({
-        percentNum: 0.1,
-      });
-    },
-    setShowSpinner() {
-      this.$loading.config({
-        showSpinner: false,
+    renderFunc() {
+      this.$message({
+        render: (h) => {
+          return h("span", [
+            "This is created by ",
+            h("span", {
+              domProps: {
+                innerHTML: "render",
+              },
+              style: {
+                color: "red",
+              },
+            }),
+            " function",
+          ]);
+        },
       });
     },
   },
 };
 </script>
 <style lang="less" scoped>
-.tb-loadingBar {
+.tb-button {
   margin: 5px;
 }
 </style>
